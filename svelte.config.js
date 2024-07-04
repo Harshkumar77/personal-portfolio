a1c49bc0c4dc4676fc286da213ec6e7a23ef2e21
volcano <volcano@example.com>
2024-07-04 20:31:18 +0530

    static

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,19 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	
+
 
 	kit: {
-		prerender : {
-			handleHttpError : 'warn'
+		prerender: {
+			handleHttpError: 'warn'
 		},
-		
+
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-
+			assets: 'build',
+			pages: 'build'
 		})
 	}
 };
